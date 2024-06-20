ac719104c602ef880376c70d9e61de15c79a20e9
mammekwa <75476253+mammekwa@users.noreply.github.com>
2024-06-20 20:37:48 +0200

    Initial Commit
    - Added Spring Boot Application

---
+++ demo/demo/src/main/java/investment_bank/example/demo/InvestmentBankApplication.java
@@ -0,0 +1,13 @@
+package investment_bank.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentBankApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/investment_bank/example/demo/InvestmentBankApplicationTests.java
@@ -0,0 +1,13 @@
+package investment_bank.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
